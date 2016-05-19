1cbf5b915902d830ed4bc4b982c2c4cfede5fbbb
huangtao <477680864@qq.com>
2016-05-19 17:49:37 +0800

    添加tag和文件

---
+++ PodProject/Classes/PodTest.h
@@ -0,0 +1,15 @@
+//
+//  PodTest.h
+//  PodProject
+//
+//  Created by fish on 16/5/19.
+//  Copyright © 2016年 huangtao. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PodTest : NSObject
+
+-(void)printTest;
+
+@end
---
+++ PodProject/Classes/PodTest.m
@@ -0,0 +1,17 @@
+//
+//  PodTest.m
+//  PodProject
+//
+//  Created by fish on 16/5/19.
+//  Copyright © 2016年 huangtao. All rights reserved.
+//
+
+#import "PodTest.h"
+
+@implementation PodTest
+
+-(void)printTest {
+    NSLog(@"牛逼的测试");
+}
+
+@end
